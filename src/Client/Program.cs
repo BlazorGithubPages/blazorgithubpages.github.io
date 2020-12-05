51733f0e3527f0714f8ea5ada9bda9d94f70138b
Jerome Bell <jeromebell@gmail.com>
2020-12-05 14:28:38 -0600

    Replaced .NET Core 3.1 Webassembly project with .NET 5;

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace BlazorPages.Client
+namespace Client
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
